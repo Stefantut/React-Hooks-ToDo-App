6919240ebd69ad2876c2f1e0ed8973a1118d2504
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-07-31 01:20:59 +0100

    Made wrapper global

--- src/App.test.js
+++ src/App.test.js
@@ -4,22 +4,25 @@ import App from "./App";
 import Todo from "./Todo";
 
 describe("Test for <App /> component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
   it("renders without crashing", () => {
     shallow(<App />);
   });
 
   it("has only one title", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find("h1.title")).toHaveLength(1);
   });
 
   it("has a div with class todo-list", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find("div.todo-list")).toHaveLength(1);
   });
 
   it("There is at least 1 todo task inside list", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Todo).length).toBeGreaterThan(1);
   });
 });
