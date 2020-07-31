14f6b74782651b546f7506a805b19d874c250669
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-07-31 01:14:22 +0100

    Added test for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import Todo from "./Todo";
 
-it("renders without crashing", () => {
-  shallow(<App />);
+describe("Test for <App /> component", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("has only one title", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1.title")).toHaveLength(1);
+  });
+
+  it("has a div with class todo-list", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div.todo-list")).toHaveLength(1);
+  });
+
+  it("There is at least 1 todo task inside list", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Todo).length).toBeGreaterThan(1);
+  });
 });
